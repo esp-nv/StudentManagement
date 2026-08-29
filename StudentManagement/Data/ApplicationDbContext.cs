51f179c06ea3388893a4df553d4fe6d9a3c9411e
esp-nv <esp_nv@abv.bg>
2026-08-29 21:22:11 +0300

    update

---
+++ StudentManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+namespace StudentManagement.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
--- StudentManagement/Program.cs
+++ StudentManagement/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Data;
+
 namespace StudentManagement
 {
     public class Program
@@ -5,6 +8,11 @@ namespace StudentManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
