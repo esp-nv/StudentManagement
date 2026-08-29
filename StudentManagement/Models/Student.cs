8aa795cf936472064c991b4b2c72f1bb4c3a9be5
esp-nv <esp_nv@abv.bg>
2026-08-29 20:29:12 +0300

    Create Student.cs

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Course { get; set; }
+
+        public int Age { get; set; }
+    }
+}
