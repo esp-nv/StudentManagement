a3db838e6c0b3b59f3be52eb47404188be5c5806
esp-nv <esp_nv@abv.bg>
2026-08-29 22:48:56 +0300

    update

---
+++ StudentManagement/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Data;
+
+namespace StudentManagement.Controllers;
+public class StudentsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public StudentsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var students = await _context.Students.ToListAsync();
+
+        return View(students);
+    }
+}
