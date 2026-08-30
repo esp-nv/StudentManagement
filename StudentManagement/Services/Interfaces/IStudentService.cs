6304f5962c6d13a939e7956e437d1975c05ad596
esp-nv <esp_nv@abv.bg>
2026-08-30 14:43:32 +0300

    upade and add Interfaces

---
+++ StudentManagement/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using StudentManagement.Models;
+
+namespace StudentManagement.Services.Interfaces;
+
+public interface IStudentService
+{
+    Task<List<Student>> GetAllAsync();
+
+    Task<Student?> GetByIdAsync(int id);
+
+    Task CreateAsync(Student student);
+
+    Task<bool> UpdateAsync(Student student);
+
+    Task<bool> DeleteAsync(int id);
+}
---
+++ StudentManagement/Services/StudentService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Data;
+using StudentManagement.Models;
+using StudentManagement.Services.Interfaces;
+
+namespace StudentManagement.Services;
+
+public class StudentService : IStudentService
+{
+    private readonly ApplicationDbContext _context;
+
+    public StudentService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Student>> GetAllAsync()
+    {
+        return await _context.Students
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<Student?> GetByIdAsync(int id)
+    {
+        return await _context.Students.FindAsync(id);
+    }
+
+    public async Task CreateAsync(Student student)
+    {
+        _context.Students.Add(student);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> UpdateAsync(Student student)
+    {
+        var existingStudent = await _context.Students
+            .FindAsync(student.Id);
+
+        if (existingStudent == null)
+        {
+            return false;
+        }
+
+        existingStudent.FirstName = student.FirstName;
+        existingStudent.LastName = student.LastName;
+        existingStudent.Email = student.Email;
+        existingStudent.Course = student.Course;
+        existingStudent.Age = student.Age;
+
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var student = await _context.Students.FindAsync(id);
+
+        if (student == null)
+        {
+            return false;
+        }
+
+        _context.Students.Remove(student);
+        await _context.SaveChangesAsync();
+
+        return true;
+    }
+}
